60ba83638c22482e15fda21ecb162d66cdae567f
EC2 Default User <ec2-user@ip-172-31-26-183.us-east-2.compute.internal>
2020-03-24 00:45:24 +0000

    removed errors

---
+++ app/controllers/admin/categories_controller_spec.rb
@@ -0,0 +1,88 @@
+require 'spec_helper'
+
+describe Admin::CategoriesController do
+  render_views
+
+  before(:each) do
+    Factory(:blog)
+    #TODO Delete after removing fixtures
+    Profile.delete_all
+    henri = Factory(:user, :login => 'henri', :profile => Factory(:profile_admin, :label => Profile::ADMIN))
+    request.session = { :user => henri.id }
+  end
+
+  it "test_index" do
+    get :index
+    assert_response :redirect, :action => 'index'
+  end
+
+  describe "test_edit" do
+    before(:each) do
+      get :edit, :id => Factory(:category).id
+    end
+
+    it 'should render template new' do
+      assert_template 'new'
+      assert_tag :tag => "table",
+        :attributes => { :id => "category_container" }
+    end
+
+    it 'should have valid category' do
+      assigns(:category).should_not be_nil
+      assert assigns(:category).valid?
+      assigns(:categories).should_not be_nil
+    end
+  end
+
+  it "test_update" do
+    post :edit, :id => Factory(:category).id
+    assert_response :redirect, :action => 'index'
+  end
+
+  describe "test_destroy with GET" do
+    before(:each) do
+      test_id = Factory(:category).id
+      assert_not_nil Category.find(test_id)
+      get :destroy, :id => test_id
+    end
+
+    it 'should render destroy template' do
+      assert_response :success
+      assert_template 'destroy'      
+    end
+  end
+
+  it "test_destroy with POST" do
+    test_id = Factory(:category).id
+    assert_not_nil Category.find(test_id)
+    get :destroy, :id => test_id
+
+    post :destroy, :id => test_id
+    assert_response :redirect, :action => 'index'
+
+    assert_raise(ActiveRecord::RecordNotFound) { Category.find(test_id) }
+  end
+
+  describe "test_create" do
+    before(:each) do
+      get :new
+    end
+
+    it 'should render template new' do
+      assert_template 'new'
+      assert_tag :tag => "table",
+        :attributes => { :id => "category_container" }
+    end
+
+    it 'should have create a new category' do
+      count1 = Category.count
+      post :new, :category => {name: 'Foobar', keywords: 'Lorem Ipsum', permalink: 'permanentLink', description: 'description'}
+      assigns(:category).should_not be_nil
+      assert assigns(:category).valid?
+      assert_response :redirect, :action => 'new'
+      expect(Category.count).to eql(count1 + 1)
+      expect(flash[:notice]).to eq("Category was successfully saved.")
+    end
+  end
+  
+end
